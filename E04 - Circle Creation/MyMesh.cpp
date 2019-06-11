6458daaf21335127397e0e0c9ad74032250b381c
Christopher <Christopher@DESKTOP-D8032OO>
2019-06-11 00:38:06 -0400

    Worked on E04. Edited my name in there, changed number of sides, and the subdivisions and AddTri section.

--- E04 - Circle Creation/MyMesh.cpp
+++ E04 - Circle Creation/MyMesh.cpp
@@ -16,6 +16,14 @@ void MyMesh::GenerateCircle(float a_fRadius, int a_nSubdivisions, vector3 a_v3Co
 		Calculate a_nSubdivisions number of points around a center point in a radial manner
 		then call the AddTri function to generate a_nSubdivision number of faces
 	*/
+	float dividAngle = 2.0f * PI / (float)a_nSubdivisions;
+
+	for (int i = 0; i < a_nSubdivisions; i++)
+	{
+		AddTri(vector3(0.0f, 0.0f, 0.0f),
+			vector3(cos(i * dividAngle), sin(i * dividAngle), 0.0f),
+			vector3(cos((i + 1) * dividAngle), sin((i + 1) * dividAngle), 0.0f));
+	}
 
 	// Adding information about color
 	CompleteMesh(a_v3Color);
