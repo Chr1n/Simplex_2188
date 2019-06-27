2e5794842a1f0dffe1ab9b59be29962c149e9f29
Christopher <Christopher@DESKTOP-D8032OO>
2019-06-26 21:55:53 -0400

    Removed some unnecessary files.

--- E07 - Gimbal Removal/AppClass.h
+++ E07 - Gimbal Removal/AppClass.h
@@ -18,7 +18,7 @@ class Application
 	quaternion m_qOrientation;
 	vector3 m_v3Rotation;
 	MyMesh* m_pMesh = nullptr;
-	String m_sProgrammer = "Alberto Bobadilla - labigm@rit.edu";
+	String m_sProgrammer = "Christopher Lawrence - cml3051@rit.edu";
 
 private:
 	static ImGuiObject gui; //GUI object
