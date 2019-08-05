011adb9cfdec8b4b49ab62b17710c6f0a764f39a
Christopher <Christopher@DESKTOP-D8032OO>
2019-08-05 15:17:36 -0400

    A05 Deliverable

--- A05 - Octree/AppClassGUI.cpp
+++ A05 - Octree/AppClassGUI.cpp
@@ -55,6 +55,8 @@ void Application::DrawGUI(void)
 			ImGui::Text("	  -: Increment Octree subdivision\n");
 			ImGui::Text("	  +: Decrement Octree subdivision\n");
 			ImGui::Separator();
+			ImGui::Text("     O: Turn On/Off Visual Representation");
+			ImGui::Separator();
 			ImGui::TextColored(ImColor(255, 255, 0), "Octree\n");
 		}
 		ImGui::End();
